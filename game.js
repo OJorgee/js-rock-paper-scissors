c7bc5537d9052f66ccc0c5042754a0ef520d90a8
Sérgio <sergiopbejamartinho@gmail.com>
2026-06-22 17:04:57 +0100

    game function

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function computerPlay() {
   return computerSelection
 }
 
-// console.log(computerPlay())
+ console.log(computerPlay())
 // uncomment to test the random pick of the array
 console.log("Your opponent has made their move, your turn...")
 
@@ -27,3 +27,42 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 console.log(playRound("paper",computerPlay()))
+
+function game(playerSelection, computerSelection)
+{
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+    const playerscore = 0
+    const computerscore = 0
+
+    for (let rounds = 0; rounds < 5; rounds ++)
+    {
+        let player = prompt("Play rock, paper or scissors!");
+        playRound(playerSelection)
+        
+        if (playRound("It's a Tie!"))
+        {
+            playerscore = playerscore
+            computerscore = computerscore
+        }
+        else if (playRound("You Win! " + playerSelection + " beats " + computerSelection))
+        {
+            playerscore += 1
+        }
+        else if ("You Lose! " + computerSelection + " beats " + playerSelection)
+        {
+            computerscore =+ 1
+        }
+
+        if((playerscore / 3) > computerscore)
+        {
+            console.Log("You Win! " + playerSelection + " beats " + computerSelection)
+        }
+        else if ((computerscore / 3) > playerscore)
+        {
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection)
+        }
+    }
+}
+
+game();
